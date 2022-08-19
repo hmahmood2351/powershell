d83161e4a3269b00b8c6d7904ffa3c2ef6fe7b04
mhm271 <hmahmood2351@protonmail.com>
2022-08-19 06:26:55 +0100

    save

--- testing.ps1
+++ testing.ps1
@@ -1,10 +1,11 @@
-$data = Get-Service | where-Object status -eq 'Stopped' | select-Object Name, Status
+# $data = Get-Service | where-Object status -eq 'Stopped' | select-Object Name, Status
 
-$data | export-csv .\testing.csv
+# $data | export-csv .\testing.csv
 
-get-content .\testing.csv | more
+# get-content .\testing.csv | more
 
 
-Get-NetIPAddress
-Get-NetIPConfiguration
-Get-DNSClientServerAddress
+# Get-NetIPAddress
+# Get-NetIPConfiguration
+# Get-DNSClientServerAddress
+
