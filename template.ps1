35aad93651a134120f4f6cb9bb30d78d650a9eac
mhm271 <hmahmood2351@protonmail.com>
2022-08-20 22:44:40 +0100

    code

---
+++ template.ps1
@@ -0,0 +1,26 @@
+<#
+
+.Synopsis
+    Short description
+
+.DESCRIPTION
+    Long description
+
+.EXAMPLE
+    Example of how to use this cmdlet
+
+.EXAMPLE
+    Another example of how to use this cmdlet 
+
+#>
+
+
+#Script Name
+#Creator
+#Date
+#Updated
+#References
+
+#Variables
+#Parameters
+#Enter tasks below as remarks
