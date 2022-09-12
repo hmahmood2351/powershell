d817948dc8acf31b5969009e3bc1f972230c6731
Mohammed Hassan Mahmood <hmahmood2351@protonmail.com>
2022-09-12 06:59:21 +0100

    ddiaudit

---
+++ QOL/PhoneDDIAudit/phoneddiaudit.ps1
@@ -0,0 +1,30 @@
+﻿#Takes in a CSV file named ddiaudit.xlsx
+#Must be in the same directory as the script file 
+#outputs a list of numbers that do not have any assigned users into txt file (by checking users actually assigned numbers on Teams Online)
+
+### USAGE EXAMPLE
+# [scriptname]
+# .\phoneddiaudit.ps1
+
+###
+
+
+
+$uncheckednumbers = import-excel ddiaudit.xlsx | select-object "Full DDI" #gets all numbers known in the spreadsheet with DDI
+$globalteamsnumbers = get-csonlineuser | select-object -Property "lineuri" #gets all known teams numbers with lineuri only
+
+
+foreach ($i in $uncheckednumbers) {
+
+}
+
+# get-csonlineuser | where-object -Property "lineuri" -like 
+
+
+
+#check 
+
+# get-csonlineuser | where-object -Property "lineuri" -like "*+44*" | select-object *
+
+
+# $globalteamsnumbers.lineuri | foreach-object {if ($_ -like "*+44*") {$_}}
--- QOL/readme.md
+++ QOL/readme.md
@@ -1,3 +1,5 @@
 # QOL
 
 Various 'production ready' QOL scripts that interact with different systems
+
+Phoneddiaudit - a script responsible for ensuring that a spreadsheet with a list of numbers has been audited properly by comparing with the Global Teams user list 
